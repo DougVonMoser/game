fbf7fd77a7b372947db77f3e4c2dd3c057bf68ef
Doug VonMoser <doug.vonmoser@gmail.com>
2019-11-29 10:12:01 -0600

    rollin

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, alignRight, centerX, centerY, column, el, fill, padding, pointer, rgb255, row, spacing, text, width)
+import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Events exposing (onClick)
@@ -22,6 +22,7 @@ type alias Model =
     { counter : Int
     , serverMessage : String
     , socketInfo : SocketStatus
+    , cards : List Card
     }
 
 
@@ -32,14 +33,33 @@ type SocketStatus
 
 
 
--- Card needs to be monoidal for the entire game
--- single souce of status
--- turned over by red, color blue whatnoot:kjkjkj
+-- Card should be monoidal for the comprehensive game
+-- who flipped, originalColor, unique Identifier yada yada
 
 
 type Card
-    = UnTurned String Team
-    | Turned String Team
+    = UnTurned Word OriginallyColored Hash
+    | Turned Word TurnedOverBy OriginallyColored Hash
+
+
+
+--unique identifier
+
+
+type Hash
+    = Hash Int
+
+
+type OriginallyColored
+    = OriginallyColored Team
+
+
+type TurnedOverBy
+    = TurnedOverBy Team
+
+
+type Word
+    = Word String
 
 
 type Team
@@ -49,7 +69,21 @@ type Team
 
 init : Int -> ( Model, Cmd Msg )
 init flags =
-    ( { counter = flags, serverMessage = "nothing clicked yet", socketInfo = Unopened }, Cmd.none )
+    ( { counter = flags
+      , serverMessage = "nothing clicked yet"
+      , socketInfo = Unopened
+      , cards = initialCards
+      }
+    , Cmd.none
+    )
+
+
+initialCards =
+    List.repeat 25 ()
+        |> List.indexedMap
+            (\x _ ->
+                UnTurned (Word "testing") (OriginallyColored Blue) (Hash x)
+            )
 
 
 
@@ -59,14 +93,49 @@ init flags =
 
 
 type Msg
-    = Clicked
+    = Clicked Hash
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        Clicked ->
-            ( { model | serverMessage = "something got clicked" }, Cmd.none )
+        Clicked hash ->
+            handleClickUpdate hash model
+
+
+handleClickUpdate clickedHash model =
+    let
+        updatedCards =
+            List.map
+                (\card ->
+                    if cardMatchesHash card clickedHash then
+                        turnOverCard Red card
+
+                    else
+                        card
+                )
+                model.cards
+    in
+    ( { model | cards = updatedCards, serverMessage = "something got clicked" }, Cmd.none )
+
+
+turnOverCard turningOverTeam card =
+    case card of
+        UnTurned word originallyColored hash ->
+            Turned word (TurnedOverBy turningOverTeam) originallyColored hash
+
+        Turned _ _ _ _ ->
+            Debug.todo "OH MY GOD"
+
+
+cardMatchesHash : Card -> Hash -> Bool
+cardMatchesHash card (Hash hash) =
+    case card of
+        UnTurned _ _ (Hash hash2) ->
+            hash == hash2
+
+        Turned _ _ _ (Hash hash2) ->
+            hash == hash2
 
 
 
@@ -78,38 +147,50 @@ update message model =
 view : Model -> Html Msg
 view model =
     Element.layout [] <|
-        column [ spacing 80, centerX, centerY ]
+        column [ width (px 1000), spacing 80, centerX, centerY ] <|
             [ el [] (text model.serverMessage)
-            , myRowOfStuff
-            , myRowOfStuff
-            , myRowOfStuff
-            , myRowOfStuff
-            , myRowOfStuff
+            , cardsView model.cards
             ]
 
 
-myRowOfStuff : Element Msg
-myRowOfStuff =
-    row [ width fill, centerY, centerX, spacing 80 ]
-        [ myElement
-        , myElement
-        , myElement
-        , myElement
-        , myElement
-        ]
+cardsView cards =
+    wrappedRow [ spacing 80 ] <| List.map cardView cards
 
 
-myElement : Element Msg
-myElement =
-    el
-        [ Background.color (rgb255 240 0 245)
-        , Font.color (rgb255 255 255 255)
-        , Border.rounded 3
-        , padding 30
-        , onClick Clicked
-        , pointer
-        ]
-        (text "stylish!")
+cardView : Card -> Element Msg
+cardView card =
+    case card of
+        UnTurned (Word word) (OriginallyColored team) hash ->
+            el
+                [ Background.color (rgb255 90 90 90)
+                , Font.color (rgb255 255 255 255)
+                , Border.rounded 3
+                , width (px 120)
+                , height (px 80)
+                , padding 30
+                , onClick <| Clicked hash
+                , pointer
+                ]
+                (text word)
+
+        Turned (Word word) (TurnedOverBy turnedOverByTeam) (OriginallyColored originallyColoredTeam) _ ->
+            el
+                [ Background.color <| getTeamColor originallyColoredTeam
+                , Border.rounded 3
+                , width (px 120)
+                , height (px 80)
+                , padding 30
+                ]
+                none
+
+
+getTeamColor team =
+    case team of
+        Red ->
+            rgb255 255 0 0
+
+        Blue ->
+            rgb255 0 0 255
 
 
 
