33c1953dbf89c2357e7ff77a8daa1eb2a564fb91
Doug VonMoser <doug.vonmoser@gmail.com>
2019-12-11 12:59:41 -0600

    peekaboo

--- assets/tests/Example.elm
+++ assets/tests/Example.elm
@@ -1,52 +1,21 @@
-module Example exposing (fuzzTest, unitTest, viewTest)
+module Example exposing (..)
 
+import Card exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode as D exposing (Decoder, at, string)
+import Json.Encode as E exposing (Value)
 import Main exposing (..)
 import Test exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (tag, text)
 
 
-{-| See <https://github.com/elm-community/elm-test>
--}
 unitTest : Test
 unitTest =
-    describe "simple unit test"
-        [ test "Inc adds one" <|
+    describe "cardDecoder"
+        [ test "decodes unturned" <|
             \() ->
-                update Inc (Model 0 "")
-                    |> Tuple.first
-                    |> .counter
-                    |> Expect.equal 1
-        ]
-
-
-{-| See <https://github.com/elm-community/elm-test>
--}
-fuzzTest : Test
-fuzzTest =
-    describe "simple fuzz test"
-        [ fuzz int "Inc ALWAYS adds one" <|
-            \ct ->
-                update Inc (Model ct "")
-                    |> Tuple.first
-                    |> .counter
-                    |> Expect.equal (ct + 1)
-        ]
-
-
-{-| see <https://github.com/eeue56/elm-html-test>
--}
-viewTest : Test
-viewTest =
-    describe "Testing view function"
-        [ test "Button has the expected text" <|
-            \() ->
-                Model 0 ""
-                    |> view
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "button" ]
-                    |> Query.first
-                    |> Query.has [ text "+ 1" ]
+                D.decodeString cardDecoder "{\"hash\": \"IMAHASH\", \"original_color\": \"gray\", \"urned_over_by\": null, \"word\": \"porro\"}"
+                    |> Expect.equal (Ok (UnTurned (Word "porro") (OriginallyColored NoTeam) (Hash "IMAHASH")))
         ]
