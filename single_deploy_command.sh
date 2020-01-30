055c44aa302d53a064e799b2f8d5df590ea8bae1
Doug VonMoser <doug.vonmoser@gmail.com>
2020-01-30 08:57:28 -0600

    you gon learn

--- single_deploy_command.sh
+++ single_deploy_command.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-docker run -v $(PWD):/workdir/yay butts bash do_deploy.sh
+#butts is the result of below
+#docker build -t butts -f Dockerfile.build_env . 
+#maybe multi stage builds could help here?
+
+docker run -v $(PWD):/workdir/yay butts bash create_release.sh
 
 #will add ec2 commands to copy relasea and whatnot
