79eb5b5795b1a3064f1ded91bf8e2ceb35f64bb4
Doug VonMoser <doug.vonmoser@gmail.com>
2020-02-23 06:08:57 -0600

    touching up deploy thing

--- embedded_script.sh
+++ embedded_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 echo "HI FROM AWS"
 cd ~
 
@@ -7,10 +9,10 @@ cd ~
 bin/first_deploy stop
 
 # find and remove all files and folders besides release and current direcotry dotfiles
-find .  -maxdepth 1 \( ! -iname "first_deploy-0.1.0.tar.gz" ! -iname ".*" \) -exec rm -rf {} \;
+sudo find .  -maxdepth 1 \( ! -iname "first_deploy-0.1.0.tar.gz" ! -iname ".*" \) -exec rm -rf {} \;
 
 # untar current release and remove it 
-tar -xvf first_deploy-0.1.0.tar.gz
+tar -xf first_deploy-0.1.0.tar.gz
 rm first_deploy-0.1.0.tar.gz
 
 # start it
